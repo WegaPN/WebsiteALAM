79ebc9bf9ac1542de13e138f520fef96c99c7b9d
WegaPN <wegapratama19@gmail.com>
2024-06-08 00:37:06 +0700

    Model

---
+++ app/Models/Jadwal.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jadwal extends Model
+{
+    use HasFactory;
+
+    protected $table = 'jadwal';
+
+    protected $fillable = [
+        'jadwalPendakian',
+        'lokasi',
+        'harga',
+        'kuota',
+    ];
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+}
---
+++ app/Models/Order.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'jadwal_id',
+        'jumlah',
+        'namaPendaki',
+        'tanggalOrder',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function jadwal()
+    {
+        return $this->belongsTo(Jadwal::class);
+    }
+
+    public function pembayaran()
+    {
+        return $this->hasMany(Pembayaran::class);
+    }
+}
