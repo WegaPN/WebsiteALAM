ccc2c1345bb0b6801dc3610c4480806f8d50309e
Kelvin Fariz <kelvinfarizrozy7@gmail.com>
2024-06-19 00:21:18 +0700

    update page peraturan

---
+++ resources/views/ketentuan/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app') 
+
+@section('content')
+<section>
+    <div class="bg">
+        <h1> KETENTUAN DAN KEWAJIBAN </h1>
+        <h4>PENDAFTAR WAJIB MEMBACA DAN MEMAHAMI SEMUA PERATURAN PENDAKIAN !!!</h4>
+        <div class="container">
+        <div class="teks">Ketentuan</div>
+        <div><p>Setiap calon pendaki harus memiliki SIMAKSI pendakian yang dikeluarkan oleh Perhutani Gunung Putri dengan ketentuan:</p></div>
+        <div><p>1. Melakukan pendaftaran di basecamp Perhutani Gunung Putri atau website resmi untuk mendapatkan izin pendakian resmi.</p></div>
+        <div><p>2. Pendaftaran menggunakan NIK sebagai ID pendaki dan upload KTP atau Kartu Keluarga bagi yang belum memiliki.</p></div>
+        <div><p>3. Usia pendaki minimal 5 (lima) tahun dan pendaki usia kurang dari 17 tahun melampirkan Surat Izin Orang Tua/Wali yang ditandatangan diatas materai dan copy identitas orang tua/wali.</p></div>
+        <div class="ticket-info">
+                <ul>
+                    <li>4. Biaya tiket pendakian per orang sudah termasuk asuransi
+                        <ul class="sub-item">
+                            <li >a. Tiket Masuk Hiking: Rp10.000,-</li>
+                            <li >b. Tiket Camping: Rp25.000,-</li>
+                            <li >c. Tiket Kendaraan: Motor Rp10.000 dan Mobil Rp20.000,-</li>  
+                        </ul>
+                    </li>
+                </ul>
+        </div>
+        <div><p>5. Pembayaran biaya tiket pendakian dilakukan sesuai dengan ketentuan yang ditetapkan Perhutani Gunung Putri dan biaya yang sudah dibayarkan tidak dapat dikembalikan</p></div>
+        <div><p>6. Perhutani Gunung Putri dapat melakukan penutupan pendakian apabila diperlukan dan sesuai dengan kebijakan Kepala Perhutani Gunung Putri</p></div>
+
+        <div class="teks2">Kewajiban</div>
+        <div><p>Setiap pendaki diwajibkan :</p></div>
+        <div><p>1. Mematuhi semua peraturan pendakian yang berlaku di Kawasan Gunung Putri.</p></div>
+        <div class="ticket-info">
+            <ul>
+                <li>2. Membawa/menggunakan perlengkapan pendakian yang sesuai dengan standar, diantaranya :
+                    <ul class="sub-item">
+                        <li>a. Tenda kedap air;</li>
+                        <li>b. Ransel/carier;</li>
+                        <li>c. Matras/alas tidur;</li>  
+                        <li>d. Kantung tidur (sleeping bag);</li>
+                        <li>e. Sarung tangan;</li>
+                        <li>f. Kaos kaki yang sesuai;</li>
+                        <li>g. Pakaian yang sesuai untuk mendaki gunung (baju, celana, sepatu, dsb);</li>
+                        <li>h. Jas hujan;</li>
+                        <li>i. Senter/lampu penerangan;</li>
+                        <li>j. Peralatan memasak sesuai kebutuhan;</li>
+                        <li>k. Obat-obatan pribadi;</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div><p>3. Membawa kantong sampah (trash bag) yang sesuai dengan ukuran yang cukup untuk setiap kelompoknya.</p></div>
+        <div><p>4. Mengisi form isian barang bawaan yang menghasilkan sampah sebelum melakukan pendakian.</p></div>
+        <div><p>5. Melakukan EVAKUASI MANDIRI kepada rekannya yang sakit dan atau mengalami kecelakaan.</p></div>
+    </div>
+    </div>
+    </section>
+    @endsection
