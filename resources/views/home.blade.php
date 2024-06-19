22b2f3197cf3db85add5953409539bf08ba65395
Deva Refika Nansyah <dichijou28@gmail.com>
2024-06-19 09:01:19 +0700

    update tampilan success login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="hero-login">
+    <div class="login-container">
+        <div class="card-header"><h2>{{ __('Dashboard') }}</h2></div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
+            <br>
+
+            {{ __('You are logged in!') }}
+        </div>
+
+        <div class="input-container">
+            <a href="{{ url('/home') }}" class="btn btn-primary">
+                {{ __('Go to Home') }}
+            </a>
         </div>
     </div>
 </div>
