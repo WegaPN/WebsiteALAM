cf4ccf7c445e036de9e1b4c2673442dac84d15a3
Deva Refika Nansyah <dichijou28@gmail.com>
2024-06-19 17:11:47 +0700

    penambahan meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="dicoding:email" content="devarefika28@gmail.com, wegapratama19@gmail.com">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
