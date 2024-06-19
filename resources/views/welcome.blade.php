bcd98b8cfea62b09d5054bf6a46ae4b2048fb302
Deva Refika Nansyah <dichijou28@gmail.com>
2024-06-19 11:46:24 +0700

    penambahan meta email

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pendakian Gunung Putri Lembang</title>
+    <meta name="dicoding:email" content="devarefika28@gmail.com, wegapratama19@gmail.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     @vite('resources/css/app.css')
 </head>
